0ebc8e69306e1a34f2110644f288ec6da55935d4
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-01-27 22:27:10 +0100

    ADD: dropdown support

--- navigation/admin.py
+++ navigation/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from navigation.models import Navigation
+from navigation.models import Navigation, NavigationDropDown
 
 
 @admin.register(Navigation)
 class NavigationAdmin(admin.ModelAdmin):
     list_display = ("name", "url", "type", "order")
+    ordering = ["order"]
+
+
+@admin.register(NavigationDropDown)
+class NavigationDropDownAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "type", "order", "parent")
     ordering = ["order"]
--- navigation/models.py
+++ navigation/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class NavigationType(models.TextChoices):
     NAV_ITEM = "nav-item"
     NAV_ITEM_DROP = "nav-item dropdown"
+    DROP_DOWN_ITEM = "dropdown-item"
 
 
 class Navigation(models.Model):
@@ -14,7 +15,33 @@ class Navigation(models.Model):
     )
     order = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        return self.name
+
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["order"], name="order must be uniqe")
+            models.UniqueConstraint(fields=["order"], name="navigation_order_unique")
+        ]
+
+    @property
+    def get_children(self):
+        return NavigationDropDown.objects.filter(parent=self)
+
+
+class NavigationDropDown(models.Model):
+    name = models.CharField(max_length=50, default="")
+    url = models.CharField(max_length=50, default="")
+    type = models.CharField(
+        max_length=50, choices=NavigationType.choices, default=NavigationType.NAV_ITEM
+    )
+    order = models.IntegerField(default=0)
+    parent = models.ForeignKey(
+        Navigation, on_delete=models.CASCADE, related_name="DropDownRelation"
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["parent", "order"], name="dropdown_order_unique"
+            )
         ]
