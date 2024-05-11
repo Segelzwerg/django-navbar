8b715ef7496626e9f578d61a041decb1b36aed45
Segelzwerg <25705862+Segelzwerg@users.noreply.github.com>
2024-05-11 23:38:39 +0200

    ADD: translation for other navi models

--- navigation/translation.py
+++ navigation/translation.py
@@ -2,7 +2,7 @@
 
 from modeltranslation.translator import register, TranslationOptions
 
-from navigation.models import Navigation
+from navigation.models import Navigation, NavigationDropDown, NavigationLogo
 
 
 @register(Navigation)
@@ -10,3 +10,17 @@ class NavigationTranslationOptions(TranslationOptions):
     """Enables translation for the navigation item names."""
 
     fields = ("name",)
+
+
+@register(NavigationDropDown)
+class NavigationTypeTranslationOptions(TranslationOptions):
+    """Enables translation for the navigation dropdown item names."""
+
+    fields = ("name",)
+
+
+@register(NavigationLogo)
+class NavigationLogoTranslationOptions(TranslationOptions):
+    """Enables translation for the navigation logo item names."""
+
+    fields = ("name", "alt_text")
