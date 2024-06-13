730e2be8162c103873f492685519a4a896ed1eba
Marcel <25705862+Segelzwerg@users.noreply.github.com>
2024-06-13 12:23:21 +0200

    Create context_processors.py

---
+++ django_navbar/context_processors.py
@@ -0,0 +1,13 @@
+"""Global context provisioner."""
+
+from django_navbar.models import Navigation, NavigationLogo
+
+
+def nav_bar(_):
+    """
+    Provides the navigation bar for all templates.
+    """
+    return {
+        "nav_urls": Navigation.objects.all().order_by("order"),
+        "nav_logo": NavigationLogo.objects.first(),
+    }
